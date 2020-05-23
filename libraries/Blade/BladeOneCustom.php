3999efa91c1f2d2a72ff6991d8d0527dd327babd
aozfen <ahmet.ozfen@gmail.com>
2020-05-24 01:15:52 +0300

    BladeOne library upgraded to version 3.44

--- libraries/Blade/BladeOneCustom.php
+++ libraries/Blade/BladeOneCustom.php
@@ -1,47 +1,50 @@
-<?php
-
+<?php /** @noinspection PhpUnused */
 
 namespace eftec\bladeone;
 
 /*
  * Its an example of a custom set of functions for bladeone.
  * in examples/TestCustom.php there is a working example
- *
  */
+
+use function array_pop;
+
 trait BladeOneCustom
 {
-
-    var $customItem=array(); // indicates the type of the current tag. such as select/selectgroup/etc.
-
+    private $customItem = []; // indicates the type of the current tag. such as select/selectgroup/etc.
 
     //<editor-fold desc="compile function">
     /**
      * Usage @panel('title',true,true).....@endpanel()
+     *
      * @param $expression
      * @return string
      */
     protected function compilePanel($expression)
     {
-        array_push($this->customItem, 'Panel');
+        $this->customItem[] = 'Panel';
         return $this->phpTag . "echo \$this->panel{$expression}; ?>";
     }
-    protected function compileEndPanel() {
-        $r=@array_pop($this->customItem);
-        if (is_null($r)) {
-            $this->showError("@endpanel","Missing @compilepanel or so many @compilepanel",true);
+
+    protected function compileEndPanel()
+    {
+        $r = @array_pop($this->customItem);
+        if ($r === null) {
+            $this->showError('@endpanel', 'Missing @compilepanel or so many @compilepanel', true);
         }
-        return " </div></section><!-- end panel -->"; // we don't need to create a function for this.
+        return ' </div></section><!-- end panel -->'; // we don't need to create a function for this.
     }
 
     //</editor-fold>
 
     //<editor-fold desc="used function">
-    function panel($title="",$toggle=true,$dismiss=true) {
+    protected function panel($title = '', $toggle = true, $dismiss = true)
+    {
         return "<section class='panel'>
                 <header class='panel-heading'>
                     <div class='panel-actions'>
-                        ".(($toggle)?"<a href='#' class='panel-action panel-action-toggle' data-panel-toggle></a>":"")."
-                        ".(($dismiss)?"<a href='#' class='panel-action panel-action-dismiss' data-panel-dismiss></a>":"")."
+                        " . (($toggle) ? "<a href='#' class='panel-action panel-action-toggle' data-panel-toggle></a>" : '') . '
+                        ' . (($dismiss) ? "<a href='#' class='panel-action panel-action-dismiss' data-panel-dismiss></a>" : '') . "
                     </div>
     
                     <h2 class='panel-title'>$title</h2>
@@ -49,6 +52,4 @@ trait BladeOneCustom
                 <div class='panel-body'>";
     }
     //</editor-fold>
-
-
 }
